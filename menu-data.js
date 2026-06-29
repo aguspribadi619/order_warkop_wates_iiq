85b54903666a1703b48cda4414af522d3274a225
Michael Pribadi <agus.pribadi619@gmail.com>
2026-06-30 00:40:23 +0700

    Update menu-data.js

--- menu-data.js
+++ menu-data.js
@@ -1,1 +1,587 @@
-
+window.STATIC_MENU = [
+  {
+    id: 1,
+    kategori: "Aneka Kopi",
+    nama_menu: "Kopi Kapal Api",
+    harga_panas: 4000,
+    harga_dingin: 0,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 2,
+    kategori: "Aneka Kopi",
+    nama_menu: "Kopi Liong",
+    harga_panas: 4000,
+    harga_dingin: 0,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 3,
+    kategori: "Aneka Kopi",
+    nama_menu: "Nestcafe Black Roast",
+    harga_panas: 5000,
+    harga_dingin: 7000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 4,
+    kategori: "Aneka Kopi",
+    nama_menu: "ABC Susu",
+    harga_panas: 5000,
+    harga_dingin: 7000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 5,
+    kategori: "Aneka Kopi",
+    nama_menu: "Indocafe Coffee Mix",
+    harga_panas: 5000,
+    harga_dingin: 7000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 6,
+    kategori: "Aneka Kopi",
+    nama_menu: "Luwak White Coffee",
+    harga_panas: 5000,
+    harga_dingin: 7000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 7,
+    kategori: "Aneka Kopi",
+    nama_menu: "Good Day",
+    harga_panas: 5000,
+    harga_dingin: 7000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 8,
+    kategori: "Aneka Kopi",
+    nama_menu: "Good Day Freeze",
+    harga_panas: 0,
+    harga_dingin: 7000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 9,
+    kategori: "Aneka Kopi",
+    nama_menu: "Good Day Cappucino",
+    harga_panas: 5000,
+    harga_dingin: 7000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 10,
+    kategori: "Aneka Kopi",
+    nama_menu: "ABC Klepon",
+    harga_panas: 5000,
+    harga_dingin: 7000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 11,
+    kategori: "Aneka Kopi",
+    nama_menu: "Caffino",
+    harga_panas: 5000,
+    harga_dingin: 7000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 12,
+    kategori: "Aneka Kopi",
+    nama_menu: "Tora Bika Cappucino",
+    harga_panas: 5000,
+    harga_dingin: 7000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 13,
+    kategori: "Aneka Kopi",
+    nama_menu: "Tora Bika Creamy Latte",
+    harga_panas: 5000,
+    harga_dingin: 7000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 14,
+    kategori: "Minuman Lain",
+    nama_menu: "Teh Manis",
+    harga_panas: 3000,
+    harga_dingin: 4000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 15,
+    kategori: "Minuman Lain",
+    nama_menu: "Jahe Merah AMH",
+    harga_panas: 5000,
+    harga_dingin: 0,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 16,
+    kategori: "Minuman Lain",
+    nama_menu: "Angetsari Susu Jahe",
+    harga_panas: 5000,
+    harga_dingin: 0,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 17,
+    kategori: "Minuman Lain",
+    nama_menu: "Es Nutrisari",
+    harga_panas: 0,
+    harga_dingin: 5000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 18,
+    kategori: "Minuman Lain",
+    nama_menu: "Es Kuku Bima",
+    harga_panas: 0,
+    harga_dingin: 5000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 19,
+    kategori: "Minuman Lain",
+    nama_menu: "Es Kuku Bima + susu",
+    harga_panas: 0,
+    harga_dingin: 7000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 20,
+    kategori: "Minuman Lain",
+    nama_menu: "Es Extra Joss",
+    harga_panas: 0,
+    harga_dingin: 5000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 21,
+    kategori: "Minuman Lain",
+    nama_menu: "Es Extra Joss + susu",
+    harga_panas: 0,
+    harga_dingin: 7000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 22,
+    kategori: "Minuman Lain",
+    nama_menu: "Milo",
+    harga_panas: 5000,
+    harga_dingin: 7000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 23,
+    kategori: "Minuman Lain",
+    nama_menu: "Ovaltine",
+    harga_panas: 5000,
+    harga_dingin: 7000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 24,
+    kategori: "Minuman Lain",
+    nama_menu: "Frisian Flag Putih",
+    harga_panas: 5000,
+    harga_dingin: 7000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 25,
+    kategori: "Minuman Lain",
+    nama_menu: "Frisian Flag Coklat",
+    harga_panas: 5000,
+    harga_dingin: 7000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 26,
+    kategori: "Minuman Lain",
+    nama_menu: "Dancow Vanilla",
+    harga_panas: 6000,
+    harga_dingin: 8000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 27,
+    kategori: "Minuman Lain",
+    nama_menu: "Dancow Cokelat",
+    harga_panas: 6000,
+    harga_dingin: 8000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 28,
+    kategori: "Minuman Lain",
+    nama_menu: "Chocolatos Cokelat",
+    harga_panas: 5000,
+    harga_dingin: 7000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 29,
+    kategori: "Minuman Lain",
+    nama_menu: "Chocolatos Matcha",
+    harga_panas: 5000,
+    harga_dingin: 7000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 30,
+    kategori: "Minuman Lain",
+    nama_menu: "Drink Beng-Beng",
+    harga_panas: 5000,
+    harga_dingin: 7000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 31,
+    kategori: "Minuman Lain",
+    nama_menu: "Teh Tarik Max Tea",
+    harga_panas: 5000,
+    harga_dingin: 7000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 32,
+    kategori: "Minuman Lain",
+    nama_menu: "Energen Coklat",
+    harga_panas: 5000,
+    harga_dingin: 7000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 33,
+    kategori: "Minuman Lain",
+    nama_menu: "Energen Vanilla",
+    harga_panas: 5000,
+    harga_dingin: 7000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 34,
+    kategori: "Minuman Lain",
+    nama_menu: "Es Soda Susu",
+    harga_panas: 0,
+    harga_dingin: 12000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 35,
+    kategori: "Minuman Lain",
+    nama_menu: "Es Soda Gembira",
+    harga_panas: 0,
+    harga_dingin: 15000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 36,
+    kategori: "Aneka Indomie",
+    nama_menu: "Indomie Goreng",
+    harga_panas: 0,
+    harga_dingin: 8000,
+    varian: ["Goreng Special", "Rendang"],
+    tersedia: true
+  },
+  {
+    id: 37,
+    kategori: "Aneka Indomie",
+    nama_menu: "Indomie Rebus",
+    harga_panas: 0,
+    harga_dingin: 8000,
+    varian: ["Soto Mie", "Kari Ayam", "Ayam Bawang"],
+    tersedia: true
+  },
+  {
+    id: 38,
+    kategori: "Aneka Indomie",
+    nama_menu: "Indomie + Telur",
+    harga_panas: 0,
+    harga_dingin: 12000,
+    varian: ["Goreng Special", "Rendang", "Soto Mie", "Kari Ayam", "Ayam Bawang"],
+    tersedia: true
+  },
+  {
+    id: 39,
+    kategori: "Aneka Indomie",
+    nama_menu: "Indomie Double",
+    harga_panas: 0,
+    harga_dingin: 16000,
+    varian: ["Goreng Special", "Rendang", "Soto Mie", "Kari Ayam", "Ayam Bawang"],
+    tersedia: true
+  },
+  {
+    id: 40,
+    kategori: "Aneka Indomie",
+    nama_menu: "Indomie Double + Telur",
+    harga_panas: 0,
+    harga_dingin: 18000,
+    varian: ["Goreng Special", "Rendang", "Soto Mie", "Kari Ayam", "Ayam Bawang"],
+    tersedia: true
+  },
+  {
+    id: 41,
+    kategori: "Aneka Indomie",
+    nama_menu: "Internet (indomie telur kornet)",
+    harga_panas: 0,
+    harga_dingin: 20000,
+    varian: ["Goreng Special", "Rendang", "Soto Mie", "Kari Ayam", "Ayam Bawang"],
+    tersedia: true
+  },
+  {
+    id: 42,
+    kategori: "Aneka Indomie",
+    nama_menu: "Pizza Mie Original",
+    harga_panas: 0,
+    harga_dingin: 20000,
+    varian: ["Goreng Special", "Rendang", "Soto Mie", "Kari Ayam"],
+    tersedia: true
+  },
+  {
+    id: 43,
+    kategori: "Menu Promo",
+    nama_menu: "Mieleleh",
+    harga_panas: 0,
+    harga_dingin: 15000,
+    varian: ["Original", "Pedas"],
+    tersedia: true
+  },
+  {
+    id: 44,
+    kategori: "Menu Promo",
+    nama_menu: "Mie Creamy",
+    harga_panas: 0,
+    harga_dingin: 15000,
+    varian: ["Original", "Pedas"],
+    tersedia: true
+  },
+  {
+    id: 45,
+    kategori: "Menu Promo",
+    nama_menu: "Ayam Sambal Matah",
+    harga_panas: 0,
+    harga_dingin: 15000,
+    varian: ["Nasi", "Goreng Special", "Rendang"],
+    tersedia: true
+  },
+  {
+    id: 46,
+    kategori: "Menu Promo",
+    nama_menu: "Ayam Katsu",
+    harga_panas: 0,
+    harga_dingin: 15000,
+    varian: ["Nasi", "Goreng Special", "Rendang"],
+    tersedia: true
+  },
+  {
+    id: 47,
+    kategori: "Menu Promo",
+    nama_menu: "Nasi Ayam Cabe Ijo",
+    harga_panas: 0,
+    harga_dingin: 15000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 48,
+    kategori: "Menu Promo",
+    nama_menu: "Mie BDG (beneran digoreng)",
+    harga_panas: 0,
+    harga_dingin: 15000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 49,
+    kategori: "Menu Promo",
+    nama_menu: "Mie Jebew (paket + es teh)",
+    harga_panas: 0,
+    harga_dingin: 15000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 50,
+    kategori: "Aneka Bubur",
+    nama_menu: "Bubur Ayam",
+    harga_panas: 0,
+    harga_dingin: 10000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 51,
+    kategori: "Aneka Bubur",
+    nama_menu: "Bubur Kacang Hijau",
+    harga_panas: 0,
+    harga_dingin: 8000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 52,
+    kategori: "Aneka Bubur",
+    nama_menu: "Bubur Ketan Hitam",
+    harga_panas: 0,
+    harga_dingin: 8000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 53,
+    kategori: "Aneka Bubur",
+    nama_menu: "Bubur Kacang Hijau + Ketan Hitam",
+    harga_panas: 0,
+    harga_dingin: 10000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 54,
+    kategori: "Cemilan",
+    nama_menu: "Rujak Cireng [isi 10]",
+    harga_panas: 0,
+    harga_dingin: 13000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 55,
+    kategori: "Cemilan",
+    nama_menu: "Piscok Lumerr [isi 5]",
+    harga_panas: 0,
+    harga_dingin: 13000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 56,
+    kategori: "Cemilan",
+    nama_menu: "Tahu Bakso [isi 5]",
+    harga_panas: 0,
+    harga_dingin: 13000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 57,
+    kategori: "Cemilan",
+    nama_menu: "Kentang Goreng",
+    harga_panas: 0,
+    harga_dingin: 13000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 58,
+    kategori: "Cemilan",
+    nama_menu: "Singkong Goreng",
+    harga_panas: 0,
+    harga_dingin: 13000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 59,
+    kategori: "Cemilan",
+    nama_menu: "Singkong Goreng Keju",
+    harga_panas: 0,
+    harga_dingin: 15000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 60,
+    kategori: "Cemilan",
+    nama_menu: "Sosis",
+    harga_panas: 0,
+    harga_dingin: 13000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 61,
+    kategori: "Cemilan",
+    nama_menu: "Jasuke",
+    harga_panas: 0,
+    harga_dingin: 7000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 62,
+    kategori: "Roti Bakar",
+    nama_menu: "Roti Bakar Bandung Mix",
+    harga_panas: 0,
+    harga_dingin: 19000,
+    varian: ["Coklat-Coklat", "Coklat-Strawberry", "Coklat-Blueberry", "Strawberry-Blueberry"],
+    tersedia: true
+  },
+  {
+    id: 63,
+    kategori: "Roti Bakar",
+    nama_menu: "Roti Bakar Bandung Mix Keju",
+    harga_panas: 0,
+    harga_dingin: 21000,
+    varian: ["Coklat", "Strawberry", "Blueberry"],
+    tersedia: true
+  },
+  {
+    id: 64,
+    kategori: "Roti Bakar",
+    nama_menu: "Roti Bakar Bandung Double",
+    harga_panas: 0,
+    harga_dingin: 23000,
+    varian: [],
+    tersedia: true
+  },
+  {
+    id: 65,
+    kategori: "Roti Bakar",
+    nama_menu: "Roti Bakar 1/2 porsi",
+    harga_panas: 0,
+    harga_dingin: 15000,
+    varian: ["Coklat", "Strawberry", "Blueberry"],
+    tersedia: true
+  }
+];
